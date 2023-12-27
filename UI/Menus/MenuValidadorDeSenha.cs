3d8befa1212773f4bf2b73003e4be51ff1ecc5ff
Montaguine <yuria.cifuentes@gmail.com>
2023-12-27 11:20:45 -0300

    Adicionou solução do exercício 8 -> Escreva um programa em C# que peça ao usuário para criar uma senha. Valide se a senha atende aos seguintes critérios: ter no mínimo 8 caracteres, conter pelo menos uma letra maiúscula, uma letra minúscula e um número. Indique se a senha é válida ou não.

--- UI/Menus/MenuValidadorDeSenha.cs
+++ UI/Menus/MenuValidadorDeSenha.cs
@@ -3,10 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ValidadorDeSenha;
 
 namespace UI.Menus
 {
     internal class MenuValidadorDeSenha
     {
+        public static void Menu()
+        {
+            int opcao;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Digite a senha que deseja validar");
+                string senha = Validador.CriaSenha();
+                if (Validador.ValidaSenha(senha))
+                {
+                    Console.WriteLine("Senha valida");
+                }
+                else
+                {
+                    Console.WriteLine("Senha invalida");
+                }
+                Console.WriteLine("\n\nDigite 0 para sair ou qualquer outro valor para continuar");
+                opcao = int.TryParse(Console.ReadLine(), out opcao) ? opcao : 0;
+            } while (opcao != 0);
+        }
     }
 }
---
+++ ValidadorDeSenha/Validador.cs
@@ -0,0 +1,47 @@
+﻿namespace ValidadorDeSenha
+{
+    public class Validador
+    {
+        public static string CriaSenha()
+        {
+            return Console.ReadLine();
+        }
+
+        public static bool ValidaSenha(string senha)
+        {
+            if (senha.Length < 8)
+            {
+                return false;
+            }
+            else
+            {
+                bool temNumero = false;
+                bool temLetraMaiuscula = false;
+                bool temLetraMinuscula = false;
+                foreach (char caracter in senha)
+                {
+                    if (char.IsNumber(caracter))
+                    {
+                        temNumero = true;
+                    }
+                    if (char.IsUpper(caracter))
+                    {
+                        temLetraMaiuscula = true;
+                    }
+                    if (char.IsLower(caracter))
+                    {
+                        temLetraMinuscula = true;
+                    }
+                }
+                if (temNumero && temLetraMaiuscula && temLetraMinuscula)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+    }
+}
